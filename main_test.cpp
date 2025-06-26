76726cc8ba86184800fa360b3b7f9575508ced07
Noemi Lainà <s311037@studenti.polito.it>
2025-06-26 11:12:00 +0200

    correzione imprecisioni

--- main_test.cpp
+++ main_test.cpp
@@ -8,7 +8,6 @@
 #include "TrovaCamminoMinimo_test.hpp"
 #include "costruzione_poliedro_test.hpp"
 #include "check_ed_vert_test.hpp"
-#include "AggiungiFaccia_test.hpp"
 #include "EsisteLato_test.hpp"
 #include "ProiezioneSfera_test.hpp"
 #include "TriangolazionePoliedro_test.hpp"
